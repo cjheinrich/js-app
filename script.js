6b2b520a2f835247b6ba851b030c24e22254a8a5
cjheinrich <cjheinrich20@gmail.com>
2022-01-18 15:28:43 -0500

    rearranged code layout
    code layout visually better to read as well added for loop

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-let pokemonList = [{name: "Charizard", height: '1.7m', type: ["fire","flying"]}, {name: "Lapras", height: '2.5m', type: ["ice","water"]},{name: "cyndaquil", height: '0.5m', type:"fire"}]
+let pokemonList = [
+    {
+        name: "Charizard",
+        height: '1.7',
+        type: ["fire","flying"]
+    },
+    {
+        name: "Lapras",
+        height: '2.5',
+        type: ["ice","water"]
+    },
+    {
+        name: "Cyndaquil",
+        height: '0.5',
+        type:["fire"]
+    }
+];
+console.log(pokemonList);
+
+
+
+for (let i = 0; i < pokemonList.length; i++)  {
+    if  (pokemonList[i].height >1){
+        document.write(pokemonList[i].name + ', ' + ('height:') + pokemonList[i].height + ' ' + "Wow! Thats a big Pokemon.")
+    }else if (pokemonList[i].height <1){
+        document.write(pokemonList[i].name + ', ' + ('height:') + pokemonList[i].height + ' ' + "Thats a small pokemon!")
+    }
+    
+}
