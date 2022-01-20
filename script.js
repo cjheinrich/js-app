924c39b54366de784943619b1acec72a6ae67cf3
cjheinrich <cjheinrich20@gmail.com>
2022-01-20 12:07:16 -0500

    update js-app
    changing  the for loop to a forEach loop

--- script.js
+++ script.js
@@ -15,15 +15,26 @@ let pokemonList = [
         type:["fire"]
     }
 ];
-console.log(pokemonList);
+
+pokemonList.forEach( (item, i, arr) => console.log(i, item, arr))
 
 
-
-for (let i = 0; i < pokemonList.length; i++)  {
-    if  (pokemonList[i].height >1){
-        document.write(pokemonList[i].name + ', ' + ('height:') + pokemonList[i].height + ' ' + "Wow! Thats a big Pokemon.")
-    }else if (pokemonList[i].height <1){
-        document.write(pokemonList[i].name + ', ' + ('height:') + pokemonList[i].height + ' ' + "Thats a small pokemon!")
-    }
     
-}
+
+
+
+
+
+
+
+// function printArrayDetails(){
+// for (let i = 0; i < pokemonList.length; i++)  {
+//     if  (pokemonList[i].height >1){
+//         document.write(pokemonList[i].name + ', ' + ('height:') + pokemonList[i].height + ' ' + "Wow! Thats a big Pokemon.")
+//     }else if (pokemonList[i].height <1){
+//         document.write(pokemonList[i].name + ', ' + ('height:') + pokemonList[i].height + ' ' + "Thats a small pokemon!")
+//     }
+// }    
+// }
+// printArrayDetails();
+// printArrayDetails();
